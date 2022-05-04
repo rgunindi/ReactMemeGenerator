1372ec1569c030acfb8f40afb624c93da814a0de
Ramazan-xcod3r <codmyhesap@gmail.com>
2022-05-04 17:49:30 +0300

    custom features

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,32 +1,24 @@
 import React from 'react';
 import data from '../data/memesData';
 export default function Meme() {
-    let meme 
+    const [meme, setMeme] = React.useState("");
     function getMeme() {
-        console.log(data.data.memes);
         const random = Math.floor(Math.random() * data.data.memes.length);
-        let memes = JSON.stringify(data.data.memes[random].url);
-        meme = memes;
-        document.getElementById('meme').src = memes;
-       // return memes;
+        let memes = (data.data.memes[random].url);
+        setMeme(memes);
     }
     return (
         <main>
-            <form className='form'>
-                <p>{meme}</p>
+            <div className='form'>
                 <input type="text" className='form--input' placeholder="Shut up" />
                 <input type="text" className='form--input' placeholder="and take my money" />
                 <button
                     onClick={getMeme}
                     className='form--button'>Get a new meme image 🖼
 
-                    </button>
-            </form>
-            <main className='meme-main'>
-                <img 
-                id='meme'
-                src="" alt='' />
-            </main>
+                </button>
+            </div>
+            <img src={meme} className='form--img' alt='' />
         </main>
     )
 }
